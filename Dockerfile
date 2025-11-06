a53eecac007939154802d338c75af6d57d0df987
cutosb <boris.cutos@fisagrp.com>
2025-11-05 19:34:09 -0500

    fix 2

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Imagen base
+#FROM adonispaz9506/holamundobase:1
+FROM python:3.14-slim
+
+
+# Establece el directorio de trabajo
+WORKDIR /app
+
+# Copia el script al contenedor
+COPY app.py .
+
+
+# Instala Flask
+RUN pip install flask
+RUN pip install requests
+
+#RUN chown -R appuser:appgroup /app
+
+#USER appuser
+# Expone el puerto en el que correrá la app
+EXPOSE 3000
+
+# Comando por defecto para ejecutar la app
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+
+def call_app_b():
+    try:
+        # Llamamos a la app B, que está en localhost:3001
+        response = requests.get('http://mi-servicio-secundario.k-att-y70-dev.svc.cluster.local:4000/')
+        return f'Respuesta de app B: {response.text}', response.status_code
+    except requests.exceptions.ConnectionError:
+        return 'Error: no se pudo conectar con app B', 500
+@app.route('/health', methods=['GET'])
+def call_health():
+    return 'OK', 200
+
+@app.route('/startup', methods=['GET'])
+def call_startup():
+    return 'OK', 200
+    
+@app.route('/readiness', methods=['GET'])
+def call_readiness():
+    return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
