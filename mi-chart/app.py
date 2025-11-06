4d2c12c662a96cafa4430d490b28a4862a319baf
cutosb <boris.cutos@fisagrp.com>
2025-11-05 20:26:14 -0500

    fixtermos

--- mi-chart/app.py
+++ mi-chart/app.py
@@ -1,18 +1,11 @@
 from flask import Flask
-import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hola desde app B', 200
 
-
-def call_app_b():
-    try:
-        # Llamamos a la app B, que está en localhost:3001
-        response = requests.get('http://mi-servicio-secundario.k-att-y70-dev.svc.cluster.local:4000/')
-        return f'Respuesta de app B: {response.text}', response.status_code
-    except requests.exceptions.ConnectionError:
-        return 'Error: no se pudo conectar con app B', 500
 @app.route('/health', methods=['GET'])
 def call_health():
     return 'OK', 200
